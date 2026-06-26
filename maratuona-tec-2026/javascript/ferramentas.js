61364dd988758c027cadbc75fff1ad8a4a5da12e
Samuel_tenório <samueltenoriodasilva@gmail.com>
2026-06-26 05:48:50 -0300

    salvar alteracoes ferramentas

---
+++ maratuona-tec-2026/javascript/ferramentas.js
@@ -0,0 +1,9 @@
+// MENU MOBILE
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("ativo");
+}
+
+// ÍNDICE LATERAL (caso queira usar botão depois)
+function toggleIndice() {
+    document.getElementById("menuIndice").classList.toggle("ativo");
+}

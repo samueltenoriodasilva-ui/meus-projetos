32ff33feab06e292f6989e7a7e6ff6ab6457a3e3
Samuel_tenório <samueltenoriodasilva@gmail.com>
2026-06-26 05:24:24 -0300

    indicie agua

--- maratuona-tec-2026/conteudo/agua.js
+++ maratuona-tec-2026/conteudo/agua.js
@@ -1,4 +1,15 @@
+// ==========================
+// MENU MOBILE
+// ==========================
 
-function toggleMenu(){
+function toggleMenu() {
     document.getElementById("menu").classList.toggle("ativo");
+}
+
+// ==========================
+// ÍNDICE
+// ==========================
+
+function toggleIndice() {
+    document.getElementById("menuIndice").classList.toggle("ativo");
 }

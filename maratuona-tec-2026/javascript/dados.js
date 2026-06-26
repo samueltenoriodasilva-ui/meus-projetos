7d66396426bfc0e0baac2a4a9f9c66c526534748
Samuel_tenório <samueltenoriodasilva@gmail.com>
2026-06-26 15:33:12 -0300

    salvar

--- maratuona-tec-2026/javascript/dados.js
+++ maratuona-tec-2026/javascript/dados.js
@@ -1,14 +1,6 @@
-// ==========================
-// MENU MOBILE
-// ==========================
-
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("ativo");
 }
-
-// ==========================
-// ÍNDICE MOBILE
-// ==========================
 
 function toggleIndice() {
     document.getElementById("menuIndice").classList.toggle("ativo");
